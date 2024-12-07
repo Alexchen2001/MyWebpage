ae8ec41cca8e79444784da91d947dfdc4dd8a8f8
Alexchen2001 <alexchen9899627@gmail.com>
2024-12-06 21:54:39 -0800

    Added Navbar

---
+++ personalwebsite/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "./components/Navbar.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
